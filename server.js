be42b2d7970890127d0f53bdfa77653b8584029d
Michael Howard <mike.gene.howard@gmail.com>
2023-04-16 18:10:55 -0400

    basic express server

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Express</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
+});
