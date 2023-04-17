27e6c88e403b9dc12bed8bc591d6e25e87117d76
Michael Howard <mike.gene.howard@gmail.com>
2023-04-16 20:43:38 -0400

    creates controllers and routes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// Route files
+import { bootcamps } from "./routes/bootcamps.js";
+
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from Express</h1>");
-});
+const logger = (req, res, next) => {
+  req.hello = "Hello World";
+  console.log("Middleware ran...");
+  next();
+};
+
+app.use(logger);
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 3000;
 
